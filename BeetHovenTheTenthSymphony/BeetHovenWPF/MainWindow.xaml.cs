b5399e37045f1268f0981d1463d0ac63c43a845f
QuackTM <QuackVX@gmail.com>
2024-11-18 13:08:50 +0100

    pianouodate

--- BeetHovenTheTenthSymphony/BeetHovenWPF/MainWindow.xaml.cs
+++ BeetHovenTheTenthSymphony/BeetHovenWPF/MainWindow.xaml.cs
@@ -1,25 +1,26 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
 
-namespace BeetHovenWPF
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// aanpassing
-    public partial class MainWindow : Window
+    namespace BeetHovenWPF
     {
-        public MainWindow()
+        public partial class MainWindow : Window
         {
-            InitializeComponent();
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
+            private void OpenPianoWindow(object sender, RoutedEventArgs e)
+            {
+                PianoWindow pianoWindow = new PianoWindow();
+                pianoWindow.Show();
+            }
         }
     }
-}
---
+++ BeetHovenTheTenthSymphony/BeetHovenWPF/PianoWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace BeetHovenWPF
+{
+    public partial class PianoWindow : Window
+    {
+        public PianoWindow()
+        {
+            InitializeComponent();
+
+            SizeChanged += PianoWindow_SizeChanged; //adjust key size on window resize
+
+            GeneratePiano();
+        }
+
+        private int _octaves = 6;
+        private const int _whiteKeyCount = 7; //niet aanpassen
+
+        private void GeneratePiano()
+        {
+            PianoCanvas.Children.Clear();
+
+            double canvasWidth = PianoCanvas.ActualWidth;
+            double canvasHeight = PianoCanvas.ActualHeight;
+
+            double whiteKeyWidth = canvasWidth / (_octaves * _whiteKeyCount);
+            double whiteKeyHeight = canvasHeight;
+            double blackKeyWidth = whiteKeyWidth * 0.6;
+            double blackKeyHeight = canvasHeight * 0.6;
+
+            string[] whiteKeysWithBlack = { "C", "D", "F", "G", "A" };
+            double currentX = 0;
+
+            for (int octave = 0; octave < _octaves; octave++)
+            {
+                for (int i = 0; i < _whiteKeyCount; i++)
+                {
+                    //current white kay
+                    Rectangle whiteKey = new Rectangle
+                    {
+                        Width = whiteKeyWidth,
+                        Height = whiteKeyHeight,
+                        Fill = Brushes.White,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 1,
+                        Tag = "White"
+                    };
+
+                    Canvas.SetLeft(whiteKey, currentX);
+                    Canvas.SetTop(whiteKey, 0);
+                    whiteKey.MouseDown += Key_MouseDown;
+                    PianoCanvas.Children.Add(whiteKey);
+
+                    //add black keys
+                    if (i < _whiteKeyCount - 1 && whiteKeysWithBlack.Contains(GetWhiteKeyName(i)))
+                    {
+                        Rectangle blackKey = new Rectangle
+                        {
+                            Width = blackKeyWidth,
+                            Height = blackKeyHeight,
+                            Fill = Brushes.Black,
+                            Tag = "Black"
+                        };
+                        
+                        Canvas.SetLeft(blackKey, currentX + whiteKeyWidth * 0.75 - (blackKeyWidth / 2));
+                        Canvas.SetTop(blackKey, 0);
+                        blackKey.MouseDown += Key_MouseDown;
+                        PianoCanvas.Children.Add(blackKey);
+                    }
+
+                    currentX += whiteKeyWidth;
+                }
+            }
+        }
+
+        private void PianoWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            GeneratePiano(); //re-generate piano keys on resize
+        }
+
+        private string GetWhiteKeyName(int index)
+        {
+            string[] whiteKeyNames = { "C", "D", "E", "F", "G", "A", "B" };
+            return whiteKeyNames[index];
+        }
+
+        private void Key_MouseDown(object sender, MouseButtonEventArgs e) //wanneer een piano noot ingedrukt wordt
+        {
+            
+        }
+    }
+}

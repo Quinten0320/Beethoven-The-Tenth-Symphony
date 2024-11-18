32c50cc0266c5a824849b7735c1365a8d33e0826
Jeroenn89 <s1174675@student.windesheim.nl>
2024-11-18 15:37:05 +0100

    Data start

---
+++ BeetHovenTheTenthSymphony/BeetHovenWPF/MidiList.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.IO.Enumeration;
+using System.Text;
+using System.Windows;
+using BeethovenDataAccesLayer;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+
+namespace BeetHovenWPF
+{
+    public partial class MidiList : Window
+    {
+        public MidiList()
+        {
+            InitializeComponent();
+            fillList();
+        }
+
+        public void fillList()
+        {
+            Data data = new Data();
+
+            List<string> midiNames = data.LoadMidiNames();
+
+            MidiFileList.ItemsSource = midiNames;
+        }
+
+        private void MidiFileList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            string selectedMidiName = MidiFileList.SelectedItem.ToString();
+
+            Data data = new Data();
+
+            try
+            {
+                MidiFile midiFile = data.LoadMidiFile(selectedMidiName);
+
+                var tempoTrack = midiFile.GetTempoMap().GetTempoChanges();
+
+
+
+                MessageBox.Show($"Naam: {selectedMidiName} BPM: {tempoTrack}", "Tempo Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
--- BeetHovenTheTenthSymphony/BeethovenDataAccesLayer/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BeethovenDataAccesLayer
-{
-    public class Class1
-    {
-
-    }
-}

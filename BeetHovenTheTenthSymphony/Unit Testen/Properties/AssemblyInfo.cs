bb39b13ddd7225aca62f166257d261abde69b5fa
Quinten <quintenwit41@gmail.com>
2024-12-03 14:27:08 +0100

    unit test file

---
+++ BeetHovenTheTenthSymphony/Unit Testen/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit Testen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unit Testen")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("032bd929-b19a-4642-aaae-b209bc9361b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BeetHovenTheTenthSymphony/Unittesten/UnitTest1.cs
@@ -0,0 +1,20 @@
+using BeethovenBusiness;
+using BeethovenDataAcces;
+
+namespace Unittesten
+{
+    public class Tests
+    {
+        private UitlezenMidiLogica uitlezenmidilogica;
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

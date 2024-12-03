0321ae38456a29b969beee558006a585f2ed4ced
Ingmar de Ruiter <94074852+IngmarDeRuiter@users.noreply.github.com>
2024-12-03 11:27:29 +0100

    Added queries for test

--- BeetHovenTheTenthSymphony/BeethovenDataAccesLayer/DataBaseHelper.cs
+++ BeetHovenTheTenthSymphony/BeethovenDataAccesLayer/DataBaseHelper.cs
@@ -23,15 +23,43 @@ namespace BeethovenDataAccesLayer
                     connection.Open();
 
                     // Create tables for the data
-                    string createHighscoreTableQuery = @"
-                        CREATE TABLE IF NOT EXISTS highscores (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            name TEXT NOT NULL
+                    string createScoreTableQuery = @"
+                        CREATE TABLE IF NOT EXISTS Score (
+                            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Score INT NOT NULL
                         );";
+
+                    //string createSongTableQuery = @"
+                    //    CREATE TABLE IF NOT EXISTS Song (
+                    //        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    //        Title TEXT NOT NULL,
+                    //        Duration INT NOT NULl,
+                    //        FilePath VARCHAR NOT NULL,
+                    //        Checkpoint INT,
+                    //        ScoreID INT,
+                    //        FOREIGN KEY (ScoreID) REFERENCES Score(ScoreID)
+                    //    );";  
+
+                    //string createFavouritesTableQuery = @"
+                    //    CREATE TABLE IF NOT EXISTS Favourites (
+                    //        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    //        SongID INT NOT NULL
+                    //        FOREIGN KEY (SongID) REFERENCES Song(SongID)
+                    //    );";
+
+                    //string createUserTableQuery = @"
+                    //    CREATE TABLE IF NOT EXISTS User (
+                    //        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    //        Name TEXT NOT NULL,
+                    //        FavouritesID INT,
+                    //        FOREIGN KEY (FavouritesID) REFERENCES Favourites(FavouritesID)
+                    //    );";
 
                     using (var command = new SQLiteCommand(connection))
                     {
-                        command.CommandText = createHighscoreTableQuery;
+                        command.CommandText = createScoreTableQuery;
+                        //command.CommandText = createSongTableQuery;
+
                         command.ExecuteNonQuery();
                     }
                 }
